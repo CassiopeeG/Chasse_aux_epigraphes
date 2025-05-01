64c406b5127cdb30d624f0462d0e32ad90a028b0
Cassiopée Gariépy <2442173@csfoy.ca>
2025-05-01 10:42:12 -0400

    Réparation des svg du footer. Débuggage du Javascript avec le domaine de Robet Blatter.

--- js/epigraphes.js
+++ js/epigraphes.js
@@ -108,6 +108,7 @@ const objJSONepigraphes = {
           "TITRE": "Maison Kerhulu, entre 1939 et 1945",
           "CREDIT": "Agathe Chiasson-Leblanc"
       },
+      "DOMAINE": "Sciences et Lettres",
       "CHASSE": {
           "CATEGORIE": "lieu",
           "INDICE": " à la maison Kerhulu.",
