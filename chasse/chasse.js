7fe981162786ee4f9299791b97c0f5f66c89646d
Cassiopée Gariépy <2442173@csfoy.ca>
2025-05-08 09:49:17 -0400

    Intégration du javascript.

---
+++ chasse/chasse.js
@@ -0,0 +1,119 @@
+/* Variables globales */
+const arrIdsPersonnagesAPiger = new Array("e0001", "e0008", "e0015", "e0019");
+const arrIdsObjetsAPiger = new Array("e0002", "e0004", "e0007", "e0021");
+const arrIdsLieuxAPiger = new Array("e0005", "e0012", "e0016", "e0022");
+const arrIndicesAValider = ["personnage", "objet", "lieu"]
+
+//*************************
+// Écouteurs d'événements 
+//*************************
+window.addEventListener("load", afficherIndice)
+window.addEventListener("load", initialiser)
+document.getElementById("btnDebuterChasse").addEventListener("click", demarrerChasse);
+document.getElementById("btnDemarrerNouvelleChasse").addEventListener("click", reactiverBoutonDebuterChasse);
+
+//*************************
+// Fonctions 
+//*************************
+
+function demarrerChasse() {
+    //Tirage au sort dans les tableaux des possibilités et mise en localStorage
+
+    //Personnages
+    let intNumeroPigee = tirerAuSort(arrIdsPersonnagesAPiger.length)
+    const strIdPersonnage = arrIdsPersonnagesAPiger[intNumeroPigee]
+    localStorage.id_personnage = strIdPersonnage
+    localStorage.personnage_est_trouve = false
+    localStorage.personnage_reponse = ""
+    //Objets
+    intNumeroPigee = tirerAuSort(arrIdsObjetsAPiger.length)
+    const strIdObjet = arrIdsObjetsAPiger[intNumeroPigee]
+    localStorage.id_objet = strIdObjet
+    localStorage.objet_est_trouve = false
+    localStorage.objet_reponse = ""
+    //Lieux
+    intNumeroPigee = tirerAuSort(arrIdsLieuxAPiger.length)
+    const strIdLieu = arrIdsLieuxAPiger[intNumeroPigee]
+    localStorage.id_lieu = strIdLieu
+    localStorage.lieu_est_trouve = false
+    localStorage.lieu_reponse = ""
+    
+    localStorage.id_chasseEnCours = "true"
+    localStorage.arrFichesVisites = ""
+    afficherIndice()
+    initialiser()
+}
+
+function afficherIndice(){
+    if(localStorage.getItem("id_chasseEnCours") == "true"){
+        //Afficher la zoneÉnigme
+        document.getElementById("zoneEnigme").removeAttribute("hidden")
+        //Afficher le segment qui parle du personnage
+        document.getElementById("segmentPersonnage").innerHTML = objJSONepigraphes[localStorage.getItem("id_personnage")].CHASSE.INDICE;
+        //Afficher le segment qui parle de l'objet
+        document.getElementById("segmentObjet").innerHTML = objJSONepigraphes[localStorage.getItem("id_objet")].CHASSE.INDICE;
+        //Afficher le segment qui parle du lieu
+        document.getElementById("segmentLieu").innerHTML = objJSONepigraphes[localStorage.getItem("id_lieu")].CHASSE.INDICE;
+    
+        // Afficher l'indice personnage
+        document.getElementById("personnageIndice").innerHTML = objJSONepigraphes[localStorage.getItem("id_personnage")].CHASSE.INDICE;
+        // Afficher l'indice objet
+        document.getElementById("objetIndice").innerHTML = objJSONepigraphes[localStorage.getItem("id_objet")].CHASSE.INDICE;
+        // Afficher l'indice lieu
+        document.getElementById("lieuIndice").innerHTML = objJSONepigraphes[localStorage.getItem("id_lieu")].CHASSE.INDICE;
+    }
+}
+    
+function tirerAuSort(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function reactiverBoutonDebuterChasse() {
+    document.getElementById("btnDebuterChasse").removeAttribute("disabled")
+}
+
+
+function initialiser() {
+    if(localStorage.getItem("id_chasseEnCours") == "true"){
+
+    //Afficher le message de chasse en cour
+    document.getElementById("messageEtatChasse").innerHTML = "Chasse en cours"
+
+    //Désactiver le bouton débuterChasse
+    document.getElementById("btnDebuterChasse").setAttribute("disabled", true)
+    //Affiher le bouton debuterNouvelleChasse
+    document.getElementById("btnDemarrerNouvelleChasse").removeAttribute("hidden")
+
+    // Afficher le lien "Chercher des indices"
+    document.getElementById("lienChercherIndices").removeAttribute("hidden")
+    }
+
+    //Boucle pour valider si un indice à été trouvé
+    for(let indiceAValider in arrIndicesAValider){
+        //Variables
+        let nomCategorie = arrIndicesAValider[indiceAValider]
+        let refIndiceEstTrouve = localStorage.getItem(nomCategorie + "_est_trouve")
+
+        //Si un indice à été trouvé
+        if(refIndiceEstTrouve == "true"){
+
+            //Afficher la rétroaction "trouvé"
+            document.getElementById(nomCategorie + "MessageTrouve").removeAttribute("hidden")
+
+            //Remplacer le "?" par la réponse
+            document.getElementById(nomCategorie + "Indice").innerHTML = localStorage.getItem(nomCategorie + "_reponse")
+        }
+    }
+
+    //Si les trois indices ont été trouvés
+    if(localStorage.getItem("personnage_est_trouve") == "true" 
+        && localStorage.getItem("objet_est_trouve") == "true"
+        && localStorage.getItem("lieu_est_trouve") == "true"){
+
+        //Enlever le message "chasse en cour"
+        document.getElementById("messageEtatChasse").setAttribute("hidden", true)
+        //Ajouter message Chasse Completé
+        document.getElementById("zoneMessageChasseCompletee").removeAttribute("hidden")
+    }
+}
+
--- fiches/fiche.js
+++ fiches/fiche.js
@@ -3,12 +3,22 @@ function obtenirValeurUrlParam(strParam) {
     return new URLSearchParams(window.location.search).get(strParam)
 }
 
+//Variable globale
 let intIdFicheCourante = obtenirValeurUrlParam("id")
 
-function initialiser(id){
-    let prenom = objJSONepigraphes[id].PRENOM
-    let nom = objJSONepigraphes[id].NOM
+//*************************
+// Écouteurs d'événements 
+//*************************
+window.addEventListener("load", initialiser);
+document.getElementById("btnSoumettre").addEventListener("click", validerPieceConviction);
+
+function initialiser(){
+    let prenom = objJSONepigraphes[intIdFicheCourante].PRENOM
+    let nom = objJSONepigraphes[intIdFicheCourante].NOM
     let nomComplet = prenom + " " + nom
+
+    // AJOUT DU LOCAL STORAGE POUR LA MAP
+    localStorage.arrFichesVisites += ", " + intIdFicheCourante 
 
     //Title
     document.querySelector("title").innerHTML = nomComplet
@@ -17,64 +27,102 @@ function initialiser(id){
     document.getElementById("nomComplet").innerHTML = nomComplet
 
     //Domaine
-    document.getElementById("domaine").innerHTML = objJSONepigraphes[id].DOMAINE
+    document.getElementById("domaine").innerHTML = objJSONepigraphes[intIdFicheCourante].DOMAINE
         
     //Image Principale Source
-    document.getElementById("url_image").src = "../assets/images/ImgGalerie/table/" + id + ".png"
+    document.getElementById("url_image").src = "../assets/images/ImgGalerie/table/" + intIdFicheCourante + ".png"
     //Image Principale Alt
     document.getElementById("url_image").alr = "Image de " + nomComplet + ".png"
 
     //Titre Image
-    document.getElementById("titre_image").innerHTML = objJSONepigraphes[id].IMAGE.TITRE
+    document.getElementById("titre_image").innerHTML = objJSONepigraphes[intIdFicheCourante].IMAGE.TITRE
 
     //Credit Image
-    document.getElementById("credit_image").innerHTML = objJSONepigraphes[id].IMAGE.CREDIT
+    document.getElementById("credit_image").innerHTML = objJSONepigraphes[intIdFicheCourante].IMAGE.CREDIT
 
     //Notes Biographiques
-    document.querySelector("#notes_biographiques > p").innerHTML = objJSONepigraphes[id].BIOGRAPHIE
+    document.querySelector("#notes_biographiques > p").innerHTML = objJSONepigraphes[intIdFicheCourante].BIOGRAPHIE
 
     //Image Localisation
-    document.getElementById("carteZoom").src = "../assets/images/imgGoogleMap/" + id + "-zoom-google-maps.png"
+    document.getElementById("carteZoom").src = "../assets/images/imgGoogleMap/" + intIdFicheCourante + "-zoom-google-maps.png"
 
     //Arrondissement
-    document.getElementById("arrondissement").innerHTML = objJSONepigraphes[id].ARRONDISSEMENT
+    document.getElementById("arrondissement").innerHTML = objJSONepigraphes[intIdFicheCourante].ARRONDISSEMENT
 
     //Quartier
-    document.getElementById("quartier").innerHTML = objJSONepigraphes[id].QUARTIER
+    document.getElementById("quartier").innerHTML = objJSONepigraphes[intIdFicheCourante].QUARTIER
     
     //Adresse
-    document.getElementById("adresse").innerHTML = objJSONepigraphes[id].ADRESSE
+    document.getElementById("adresse").innerHTML = objJSONepigraphes[intIdFicheCourante].ADRESSE
 
     // Épigraphe Source
-    document.getElementById("url_plaque").src = "../assets/images/epigraphes/epigraphe_" + id + ".png"
+    document.getElementById("url_plaque").src = "../assets/images/epigraphes/epigraphe_" + intIdFicheCourante + ".png"
     // Épigraphe Alt
     document.getElementById("url_plaque").alt = "Épigraphe de " + nomComplet
 
     //Transcription
-    document.getElementById("transcript").innerHTML = objJSONepigraphes[id].PLAQUE_TRANSCRIPTION
+    document.getElementById("transcript").innerHTML = objJSONepigraphes[intIdFicheCourante].PLAQUE_TRANSCRIPTION
 
     //Capsule Sonore URL
-    document.getElementById("audio_url").src = objJSONepigraphes[id].AUDIO.URL
+    document.getElementById("audio_url").src = objJSONepigraphes[intIdFicheCourante].AUDIO.URL
 
     //Capsule Sonore Preambule
-    document.getElementById("audio_preambule").innerHTML = objJSONepigraphes[id].AUDIO.DESCRIPTION
+    document.getElementById("audio_preambule").innerHTML = objJSONepigraphes[intIdFicheCourante].AUDIO.DESCRIPTION
     
     //Capsule Sonore Transcription
-    document.getElementById("audio_transcription").innerHTML = objJSONepigraphes[id].AUDIO.TRANSCRIPTION
+    document.getElementById("audio_transcription").innerHTML = objJSONepigraphes[intIdFicheCourante].AUDIO.TRANSCRIPTION
     
     //Capsule Sonore Credit
-    document.getElementById("audio_credit").innerHTML = objJSONepigraphes[id].AUDIO.CREDIT
+    document.getElementById("audio_credit").innerHTML = objJSONepigraphes[intIdFicheCourante].AUDIO.CREDIT
     
     //Capsule Sonore Credit
-    document.getElementById("audio_credit").innerHTML = objJSONepigraphes[id].AUDIO.CREDIT
+    document.getElementById("audio_credit").innerHTML = objJSONepigraphes[intIdFicheCourante].AUDIO.CREDIT
 
-    
+    if(localStorage.id_chasseEnCours == "true"){
+        document.getElementById("zoneAucuneChasseEnCours").hidden = true
+        document.getElementById("zoneChasseEnCours").hidden = false
+
+        
+    }
 }
 
 obtenirValeurUrlParam()
 
-initialiser(intIdFicheCourante)
 
+function validerPieceConviction() {
+    const refRadioCoche = document.querySelector('[name="formChasse"]:checked');
+    const refMessage = document.getElementById('message'); 
 
+    //Si bouton pas coché
+    if(refRadioCoche == null){
+        refMessage.innerHTML = "Veuillez sélectionner un élément."
+    } //Si bonne réponse
+    else if(intIdFicheCourante == localStorage.getItem("id_" + refRadioCoche.value)){
+        //Variables locales
+        const categorie = refRadioCoche.value
+        let strReponse = objJSONepigraphes[intIdFicheCourante].CHASSE.REPONSE
+        let strPersonnageTrouvé = objJSONepigraphes[intIdFicheCourante].PRENOM + " " + objJSONepigraphes[intIdFicheCourante].NOM
+        let indiceTrouve = categorie + "_est_trouve"
+
+        //Affiche la réponse
+        refMessage.innerHTML = "Bravo! Vous avez trouvé " + strReponse 
+        //Change [l'indice]_est_trouve dans le local storage pour true
+        localStorage.setItem(indiceTrouve, "true")
+        //Mettre la reponse dans le localStorage pour afficher dans la chasse
+        localStorage.setItem(categorie + "_reponse", strPersonnageTrouvé)
+
+        //Si c'est le dernier indice trouvé
+        if(localStorage.getItem("personnage_est_trouve") == "true" 
+        && localStorage.getItem("objet_est_trouve") == "true"
+        && localStorage.getItem("lieu_est_trouve") == "true"){
+
+            refMessage.innerHTML = "Bravo! Vous avez terminé la chasse, vous pouvez maintenant participer au Concours!"
+        }
+    }
+    //Si mauvaise réponse
+    else{ 
+        refMessage.innerHTML = "Désolé. Ce n’est pas le bon élément."
+    }
+}
 
 
